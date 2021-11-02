ce362c245ad4e131b2e34e4341cf100e89638ee5
EPro30 <82483823+EPro30@users.noreply.github.com>
2021-11-01 23:41:42 -0400

    Initial commit

---
+++ src/test/java/ucf/assignments/FXMLControllerTest.java
@@ -0,0 +1,14 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solution
+ *  Copyright 2021 Riya Singh
+ */
+package ucf.assignments;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FXMLControllerTest {
+
+    //Create tests to check expected items in arrayList (includes check for rename of title)
+    //Create tests to check the file in the expected external storage
+
+}
---
+++ src/test/java/ucf/assignments/itemsTest.java
@@ -0,0 +1,14 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solution
+ *  Copyright 2021 Riya Singh
+ */
+package ucf.assignments;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class itemsTest {
+
+    //Create tests to check expected items in observable lists
+    //The test includes a check for adding events in file, renaming date and description, status of tasks
+
+}
